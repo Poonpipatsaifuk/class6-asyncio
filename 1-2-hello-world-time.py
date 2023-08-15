9649eba160a594262b1e4678e17de294b94b74cb
Poonpipatsaifuk <127715230+Poonpipatsaifuk@users.noreply.github.com>
2023-08-15 17:50:51 +0700

    Update 1-2-hello-world-time.py

--- 1-2-hello-world-time.py
+++ 1-2-hello-world-time.py
@@ -1,38 +1,17 @@
-"""
-ต้องการซักผ้า 2 ตะกร้า แบบ asynchronous io
-กระบวนการซักผ้า 1 ตะกร้า คือ
-1. หยอดเหรียญเพื่อซักผ้า
-2. นำผ้าเข้าเครื่องซักผ้า
-3. ซักผ้าเสร็จ (ใช้เวลา 5 วินาที)
-
-เนื่องจากมีเครื่องซักผ้าที่สามารถพร้อมใช้งานได้ 2 เครื่องพร้อมกัน 
-
-เปลี่ยนการทำงานเป็นแบบ asynchronous io 
-"""
-
+import asyncio
 import time
 
-import asyncio
-
-async def wash(basket):
-    print(f'Washing Machine ({basket}): Put the coin')
-    print(f'Washing Machine ({basket}): Start washing...')
-    await asyncio.sleep(5)
-    print(f'Washing Machine ({basket}): Finished washing')
-    return f'{basket} is completed'
+async def example(message):
+    print(f"{time.ctime()} - start of :", message)
+    await asyncio.sleep(1)
+    print(f"{time.ctime()} - end of :", message)
 
 async def main():
-    coro = wash('Basket A')
-    print(coro)
-    print(type(coro))
-    task = asyncio.create_task(coro)
-    print(task)
-    print(type(task))
-    result = await task
-    print(result)
+    # Start coroutine twice (hopefully they start!)
+    first_awaitable = example("First call")
+    second_awaitable = example("Second call")
+    # Wait for coroutines to finish
+    await first_awaitable
+    await second_awaitable
 
-if __name__ == '__main__':
-    t1 = time.time()
-    asyncio.run(main())
-    t2 = time.time() - t1
-    print(f'Executed in {t2:0.2f} seconds.')
+asyncio.run(main())
