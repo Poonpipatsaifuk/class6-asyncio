406b5808476040eeb4ad7ac63d25005ba8f996fd
Poonpipatsaifuk <127715230+Poonpipatsaifuk@users.noreply.github.com>
2023-08-15 17:50:26 +0700

    Update 1-1-hello-world-wrong.py

--- 1-1-hello-world-wrong.py
+++ 1-1-hello-world-wrong.py
@@ -1,28 +1,18 @@
-"""
-ต้องการซักผ้า 2 ตะกร้า แบบ synchronous io
-กระบวนการซักผ้า 1 ตะกร้า คือ
-1. หยอดเหรียญเพื่อซักผ้า
-2. นำผ้าเข้าเครื่องซักผ้า
-3. ซักผ้าเสร็จ (ใช้เวลา 5 วินาที)
-
-เนื่องจากมีเครื่องซักผ้าที่สามารถพร้อมใช้งานได้ 2 เครื่องพร้อมกัน 
-"""
-
+# When do coroutines start running?
+import asyncio
 import time
 
-def wash(basket):
-    print(f'{time.ctime()} - Washing Machine ({basket}): Put the coin')
-    print(f'{time.ctime()} - Washing Machine ({basket}): Start washing...')
-    time.sleep(5)
-    print(f'{time.ctime()} - Washing Machine ({basket}): Finished washing')
-    return f'{basket} is completed'
+async def print_after(message, delay):
+    """Print a message after the specified delay (in seconds)"""
+    await asyncio.sleep(delay)
+    print(f"{time.ctime()} - {message}")
 
-def main():
-    for basket in ['Basket A', 'Basket B']:
-        wash(basket)
+async def main():
+    # Start coroutine twice (hopefully they start!)
+    first_awaitable = print_after("world", 2)
+    second_awaitable = print_after("world", 1)
+    # Wait for coroutines to finish
+    await first_awaitable
+    await second_awaitable
 
-if __name__ == '__main__':
-    t1 = time.time()
-    main()
-    t2 = time.time() - t1
-    print(f'Executed in {t2:0.2f} seconds.')
+asyncio.run(main())
